6a66967a7c0564d8f70eed9acdebc35e9cdfd979
Oleg Chiruhin <hedin.pr@gmail.com>
2011-09-29 01:20:30 +0700

    add module for packaging of executable jar

---
+++ ru.nextspeed.organizer.ui/src/main/java/ru/nextspeed/organizer/ui/EntryPoint.java
@@ -0,0 +1,14 @@
+package ru.nextspeed.organizer.ui;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: olegchir
+ * Date: 9/29/11
+ * Time: 12:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EntryPoint {
+    public static void main(String[] args) {
+        System.out.println("Hello, Nextspeed!");
+    }
+}
